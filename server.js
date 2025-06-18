6d37fec666c229b31de351c9ea7f512f394339b9
Phanurat <paunavt5@gmail.com>
2025-06-18 13:31:43 +0700

    Update Route and file path

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Redirect root '/' to Login Page
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+// Redirect root '/' to Login Page
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'register.html'));
+});
+
 // Start server on port 3000
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
